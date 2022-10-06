2caf08a0b861161f42adb07cc826fa2f9667fcb2
Alexandre Borges <alexandreborges.goncalves@gmail.com>
2022-10-06 19:04:37 -0300

    introducao reactjs

---
+++ react/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export function Home() {
+
+  return (
+    
+    <div className='container'>
+    <h1>Hello World</h1>
+    <h2>Aaron Judge</h2>
+
+    </div>
+  )
+}
+
+
